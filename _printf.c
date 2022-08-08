64d3517a9e10b23409413b407dddf886cf872ea4
Blessing Udiong <udiongblessing@gmail.com>
2022-08-08 16:16:13 +0100

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - produces an output according to a given format
+ * @format: The format to be printed out to stdout
+ *
+ * Return: The length of the string printed to stdout
+ */
+
+int _printf(const char *format, ...)
+{
+        va_list args;
+        int i, count;
+        int (*ptr)(va_list);
+
+        va_start(args, format);
+        count = 0;
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] != '%')
+                {
+                        _putchar(format[i]);
+                        count++;
+                }
+                else
+                {
+                        ptr = get_func(format[i + 1]);
+                        if (ptr)
+                        {
+                                count += ptr(args);
+                                i++;
+                        }
+                        else
+                                count += _putchar('%');
+                }
+        }
+        va_end(args);
+        return (count);
+}
